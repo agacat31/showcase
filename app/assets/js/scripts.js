96306d493fcb338e786e3e5fb4f79460be5cbaa7
aga <aga@adeptforms.com>
2018-04-07 22:25:13 +0700

    First Commit

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,29 @@
+/*!
+ * Adeptforms
+ * We Automate Every Process Paper to Paperless
+ * https://adeptforms.com
+ * @author Aga Atmaja
+ * @version 1.0.5
+ * Copyright 2018. MIT licensed.
+ */
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 60) {
+      $("#mainNav").addClass("navbar-shrink");
+      $("#mainNav .navbar-brand img.black").hide();
+      $("#mainNav .navbar-brand img.white").show();
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+      $("#mainNav .navbar-brand img.white").hide();
+      $("#mainNav .navbar-brand img.black").show();
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+})(jQuery); // End of use strict
